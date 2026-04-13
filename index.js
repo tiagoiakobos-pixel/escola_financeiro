2309dcc98af93314906f170823790b3698ead6fa
tiagoiakobos-pixel <tiagoiakobos@gmail.com>
2026-04-13 15:57:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API rodando 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
